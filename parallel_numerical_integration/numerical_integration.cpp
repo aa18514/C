ea024407cb5a1d04936f5b0067fffd9b493f738e
aa18514 <aa18514@ic.ac.uk>
2017-11-01 20:47:36 +0000

    enhanced parallel_numerical_integration

--- parallel_numerical_integration/numerical_integration.cpp
+++ parallel_numerical_integration/numerical_integration.cpp
@@ -8,17 +8,8 @@
 #include <vector>
 #include <stdio.h>
 #include <stdlib.h>
-#include "parallel_accumulator.h"
 
-typedef int Item;
-
-struct Node{
-	Item dataSize;	
-	double* data; 
-	Node* Next; 
-};
-
-typedef Node* NodePtr;
+using namespace std; 
 
 double naive_implementation(long double num_steps, int experiments);
 double multi_core_implementation(long double num_steps, double scaling_factor, float total_experiments, int C); 
@@ -35,8 +26,8 @@ int main(int argc, char* argv[])
 	else{
 		naive_answer = naive_implementation(strtod(argv[1], NULL), atoi(argv[2])); 
 		multi_core_answer = multi_core_implementation(strtod(argv[3], NULL), atof(argv[4]), atoi(argv[5]), atoi(argv[6])); 
-		cout << multi_core_answer << endl;
-		cout << naive_answer << endl;
+		printf("evaluated answer on multi-core: %f\t\t", float(multi_core_answer));
+		printf("expected answer: %f\n", float(naive_answer));
 	}
 	return 0; 
 }
@@ -75,65 +66,42 @@ inline double multi_core_implementation(long double num_steps, double scale_fact
 		gettimeofday(&tv, NULL); 
 		int iterations = int(double(scale_factor) * double(num_steps));
 		int size = (1.0/double(scale_factor));
-		NodePtr hdlist = new Node;
-		for(int i = 0; i < 6; i++){
-			
-			cout << i << endl;
-			int index = (i == 0) ? 2 : 
-						i + 3; 	 
-			if(i == 0){
-				hdlist -> data = new double[iterations >> index];
-				hdlist -> dataSize = iterations >> i;
-				hdlist -> Next = NULL;
-			}
-			else{
-				NodePtr nextNode = new Node;
-				nextNode -> data = new double[iterations >> index];
-				nextNode -> dataSize = int(iterations >> i);
-				nextNode -> Next = hdlist;
-				hdlist = nextNode;
-			}
-
-		} 
-		cout << "MERRY CHRISTMAS" << endl;
 		double* level_up_sums = new double[size];
-
-		int iterations_factor = hdlist -> Next -> Next -> Next -> Next -> dataSize % 2; 
-		int iterations_level_up_factor = hdlist -> Next -> Next -> Next -> dataSize % 2; 
-		int iterations_two_level_up_factor = (hdlist -> Next -> Next -> dataSize) % 2;
-		int iterations_three_level_up_factor = hdlist -> Next -> Next -> dataSize % 2;
-		int iterations_four_level_up_factor = hdlist -> dataSize % 2;   
-
+		vector<double*> partial_sum;
+		double* level_two_up_sums = NULL;
+		double* level_three_up_sums = NULL; 
+		double* level_four_up = NULL;
+		double*	level_five_up = NULL; 
+		double* level_six_up = NULL;
+		int index; 
+		int i = 0; 
+		for(i = 0; i < 6; i++){
+			index = (i == 0) ? 2 : i + 2; 
+			partial_sum.push_back(new double[iterations >> index]);
+		}
+	
 		for(int64_t j = 0; j < size; j++)
 		{
-			bring_data_in_block(hdlist -> Next -> Next -> Next -> Next -> Next -> data, j * hdlist -> Next-> Next -> Next -> Next -> Next -> dataSize, hdlist -> Next -> Next -> Next -> Next -> Next -> dataSize, step);
+			bring_data_in_block(partial_sum[0], j * iterations, iterations, step);
+			for(int i = 0; i < 5; i++){
+				int index = (i == 0) ? 0 : i + 2; 
+				level_up_sums[j] += ((iterations >> index) % 2 == 0) ? 0 : partial_sum[i][(iterations >> index) - 1]; 
+				reduce(partial_sum[i + 1], partial_sum[i], iterations >> (i + 1)); 
+			}
+			level_up_sums[j] += ((iterations >> 6) > 1) ? accumulate(partial_sum[5], partial_sum[5] + ((iterations >> 7)), double(0)) : 
+					    ((iterations >> 5) > 1) ? partial_sum[4][0] : 
+					    ((iterations >> 4) > 1) ? partial_sum[3][0] :
+					    ((iterations >> 3) > 1) ? partial_sum[2][0] : 
+					    ((iterations >> 2) > 1) ? partial_sum[1][0] : 
+					    partial_sum[0][0];
 	
-			level_up_sums[j] += (!iterations_factor) ? 0 : hdlist -> data[(hdlist -> Next -> Next -> Next -> Next -> Next -> dataSize) >> 2 - 1];
-			reduce(hdlist -> Next -> Next -> Next -> Next -> data , hdlist -> Next -> Next -> Next -> Next -> Next -> data, hdlist -> Next -> Next -> Next -> Next -> dataSize);
-			
-			level_up_sums[j] += (!iterations_level_up_factor) ? 0 : hdlist->Next->Next->Next->data[(hdlist->Next->Next->Next->dataSize) >> 2 - 1];
-			reduce(hdlist -> Next -> Next -> Next -> data, hdlist -> Next -> Next -> Next->Next->data, hdlist -> Next -> Next -> Next -> Next -> dataSize);
-			
-			level_up_sums[j] += (!iterations_two_level_up_factor) ? 0 : hdlist->Next->Next->data[(hdlist -> Next -> Next -> dataSize) >> 2 - 1];
-			reduce(hdlist -> Next -> Next -> data, hdlist -> Next -> Next -> Next -> data, hdlist -> Next -> Next -> Next -> dataSize);
-			
-			level_up_sums[j] += (!iterations_three_level_up_factor) ? 0 : hdlist->Next->Next->Next->data[(hdlist -> Next -> dataSize) >> 2 - 1];
-			reduce(hdlist -> Next -> data, hdlist -> Next -> data, hdlist -> Next -> Next -> dataSize);
-			
-			level_up_sums[j] += (!iterations_four_level_up_factor) ? 0 : hdlist->data[(hdlist -> dataSize) >> 2 - 1];
-			reduce(hdlist -> data, hdlist->Next->data, hdlist->dataSize);
-	
-			level_up_sums[j] += (hdlist -> dataSize > 1) ? accumulate(hdlist -> data, hdlist -> data + hdlist -> dataSize, double(0)) : 
-					    (hdlist -> Next -> dataSize > 1) ? hdlist -> Next -> data[0] : 
-					    (hdlist -> Next -> Next -> dataSize > 1) ? hdlist -> Next -> Next -> data[0] :
-					    hdlist -> Next -> Next -> Next -> data[0];
 		}
+		
 		
 		sum = (size == 1) ? level_up_sums[0] : 
 			((size % 2) == 0) ? 0 : level_up_sums[size - 1];
 		if(size > 1){
-			int accumulate_index = 0;
-			double level_two_up_sums[size >> 1];
+			level_two_up_sums = new double[size >> 1];
 			#pragma omp parallel for
 			for(int i = 0; i < size; i = i + 2){
 				level_two_up_sums[i >> 1] = level_up_sums[i] + level_up_sums[i + 1];
@@ -143,8 +111,8 @@ inline double multi_core_implementation(long double num_steps, double scale_fact
 			sum = (size_level_two_up == 1) ? sum + level_two_up_sums[0] : 
 				((size_level_two_up % 2) == 0) ? sum : level_two_up_sums[size_level_two_up - 1];
 			size_level_two_up = ((size_level_two_up % 2) == 0) ? size_level_two_up : size_level_two_up - 1;
-			double level_three_up_sums[size_level_two_up >> 1];
 			if(size_level_two_up > 1){
+				level_three_up_sums = new double[size_level_two_up >> 1];
 				#pragma omp parallel for
 				for(int i = 0; i < size_level_two_up; i = i + 2){
 					level_three_up_sums[i >> 1] = level_two_up_sums[i] + level_two_up_sums[i + 1];
@@ -155,8 +123,8 @@ inline double multi_core_implementation(long double num_steps, double scale_fact
 			sum = (size_level_three_up == 1) ? sum + level_three_up_sums[0]: 
 				((size_level_three_up % 2) == 0) ? sum : level_three_up_sums[size_level_three_up - 1];
 			size_level_three_up = ((size_level_three_up % 2) == 0) ? size_level_three_up : size_level_three_up - 1;
-			double level_four_up[size_level_three_up >> 1];
 			if(size_level_three_up > 1){
+				level_four_up = new double[size_level_three_up >> 1];
 				#pragma omp parallel for 
 				for(int i = 0; i < size_level_three_up; i = i + 2){
 					level_four_up[i >> 1] = level_three_up_sums[i] + level_three_up_sums[i + 1];
@@ -167,8 +135,8 @@ inline double multi_core_implementation(long double num_steps, double scale_fact
 			sum = (size_level_four_up == 1) ? sum + level_four_up[0] : 
 				((size_level_four_up % 2) == 0) ? sum : level_four_up[size_level_four_up - 1];
 			size_level_four_up = ((size_level_four_up % 2) == 0) ? size_level_four_up : size_level_four_up - 1;
-			double level_five_up[size_level_four_up >> 1];
 			if(size_level_four_up > 1){
+				level_five_up = new double[size_level_four_up >> 1]; 
 				#pragma omp parallel for 
 				for(int i = 0; i < size_level_four_up; i = i + 2){
 					level_five_up[i >> 1] = level_four_up[i] + level_four_up[i + 1]; 
@@ -179,14 +147,14 @@ inline double multi_core_implementation(long double num_steps, double scale_fact
 			sum = (size_level_five_up == 1) ? sum + level_five_up[0] : 
 				((size_level_five_up % 2) == 0) ? sum : level_five_up[size_level_five_up - 1];
 			size_level_five_up = ((size_level_five_up % 2) == 0) ? size_level_five_up : size_level_five_up - 1;
-			double level_six_up[size_level_five_up >> 1];
 			if(size_level_five_up > 1){
+				level_six_up = new double[size_level_five_up >> 1];
 				#pragma omp parrallel for
 				for(int i = 0; i < size_level_five_up; i = i + 2){
 					level_six_up[i >> 1] = level_five_up[i] + level_five_up[i + 1];
 				}
-				for(accumulate_index = 0; accumulate_index < size_level_five_up >> 1; accumulate_index++){
-					sum = sum + level_six_up[accumulate_index];
+				for(i = 0; i < (size_level_five_up >> 1); i++){
+					sum = sum + level_six_up[i];
 				}
 			}
 		}
